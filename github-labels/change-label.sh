ffef0622c5164400f6ce587bcede7965f19094cf
Matt Pennington <matthew.pennington@colorado.edu>
2020-08-17 12:02:01 -0600

    fixed typo in var name, updated api, added documentation #146

--- github-labels/change-label.sh
+++ github-labels/change-label.sh
@@ -12,7 +12,7 @@ else
   oldLabel=$1
   newLabel=$2
   newColor=$3
-  new={\"name\":\"${newLabel}\",\"color\":\"${newColor}\"}
+  new={\"new_name\":\"${newLabel}\",\"color\":\"${newColor}\"}
 fi
 
 binDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -20,19 +20,23 @@ creds=`node ${binDir}/printGithubAuthorization.js`
 ./update-repos-list.sh
 
 echo 'For each repo, this script should print "200 OK" to indicate success'
+echo 'If a 404 Not Found is printed, that repo is likely missing the standard label set.'
+echo 'If a 403 Forbidden is printed, that repo has probably been archived and this is correct.  If you believe the repo should have been updated, check that your Github OAuth token is correct.'
+echo ''
 
 for repo in `cat .repos`
 do
   repo=`echo ${repo}`
-  url=https://api.github.com/repos/phetsims/${repo}/labels/${old}
+  url=https://api.github.com/repos/phetsims/${repo}/labels/${oldLabel}
   echo "Path: ${url}"
   echo "Data: ${new}"
   echo "Result:"
   curl -isH 'User-Agent: "PhET"' -u "$creds" -d "$new" -X PATCH "$url" | head -n 1
+  echo ''
 done
 
 sed s/${oldLabel}.*/${newLabel},${newColor}/ github-labels > .tmp && mv .tmp github-labels
 sort github-labels -o github-labels
-git pull && git commit github-labels -m "Changed github label from ${old} to ${new}" && git push
+git pull && git commit github-labels -m "Changed github label from ${oldLabel} to ${new}" && git push
 
 echo "Complete."
