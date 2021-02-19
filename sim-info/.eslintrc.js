5c6c7cfe80618a9f21b3aca1f5b5511acc1f3981
samreid <reids@colorado.edu>
2021-02-18 20:40:52 -0700

    Output repos by developer, see https://github.com/phetsims/special-ops/issues/185

---
+++ sim-info/.eslintrc.js
@@ -0,0 +1,10 @@
+// Copyright 2018, University of Colorado Boulder
+// @author Michael Kauzmann
+
+'use strict';
+
+module.exports = {
+
+  // Use all of the default rules from eslint file for node code.
+  'extends': '../../chipper/eslint/node_eslintrc.js'
+};
