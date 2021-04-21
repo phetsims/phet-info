c6a2a5dbffb02db4b8b6b7d65c32220bd2bd1428
zepumph <michael.kauzmann@colorado.edu>
2021-04-21 14:05:04 -0800

    reformat responsible_dev.json, delete md file, create script to check against what is on github currently, https://github.com/phetsims/special-ops/issues/185

---
+++ sim-info/getAllRepos.js
@@ -0,0 +1,54 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * Get all repos in phetsims that are not archived, disabled, or forks. This probably won't work unless you have
+ * admin privileges to read all repos.
+ */
+
+const buildLocal = require( '../../perennial/js/common/buildLocal' );
+const https = require( 'https' );
+
+const getSomeRepos = async pageNumber => {
+  return new Promise( resolve => {
+
+    const requestOptions = {
+      host: 'api.github.com',
+      path: `/orgs/phetsims/repos?per_page=100&page=${pageNumber}`,
+      method: 'GET',
+      auth: `${buildLocal.developerGithubUsername}:${buildLocal.developerGithubAccessToken}`,
+      headers: {
+        'User-Agent': 'PhET'
+      }
+    };
+
+    const request = https.request( requestOptions, res => {
+
+      let data = '';
+      res.on( 'data', d => {
+        data += d.toString();
+      } );
+
+      res.on( 'end', () => {
+        const repoData = JSON.parse( data.toString() );
+        resolve( repoData.filter( x => {
+          return !x.archived && !x.disabled && !x.fork; // no repos that are archived, disabled, or actually just forks
+        } ).map( x => x.name ) );
+      } );
+    } );
+
+    request.on( 'error', e => { throw e; } );
+    request.end();
+  } );
+};
+
+/**
+ * @returns {Promise<string[]>}
+ */
+module.exports = async function() {
+  let repos = [];
+
+  for ( let i = 1; i < 5; i++ ) {
+    repos = repos.concat( await getSomeRepos( i ) );
+  }
+  return repos;
+};
