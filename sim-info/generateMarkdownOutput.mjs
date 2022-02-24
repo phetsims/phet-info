3983644fbee72330ccb258c8aee402f073fda439
zepumph <michael.kauzmann@colorado.edu>
2022-02-24 10:45:42 -0700

    add features to responsible_dev.md, https://github.com/phetsims/phet-info/issues/179

--- sim-info/generateMarkdownOutput.mjs
+++ sim-info/generateMarkdownOutput.mjs
@@ -20,14 +20,40 @@ let responsibleTableString = `
 NOTE: This file is generated, do not edit directly. It is created from \`responsible_dev.json\`, see \`./generateMarkdownOutput.mjs\`.
 
 
-| Simulation  | Developer | Designer |
-| :---------- | :------------- | :------------- |
+| Simulation  | Developer | Designer | Features |
+| :---------- | :------------- | :------------- | :------------- |
 `;
+
+
+// look at the repo's package.json in phet.simFeatures and note selected features for the sim.
+const getFeatures = repo => {
+  const features = [];
+  try {
+    const packageJSON = JSON.parse( fs.readFileSync( `./${repo}/package.json` ).toString() );
+    if ( packageJSON.phet && packageJSON.phet.simFeatures ) {
+      if ( packageJSON.phet.simFeatures.supportsSound ) {
+        features.push( 'Sound' );
+      }
+      if ( packageJSON.phet.simFeatures.supportsInteractiveDescription ) {
+        features.push( 'Interactive Description' );
+      }
+      if ( packageJSON.phet.simFeatures.supportsVoicing ) {
+        features.push( 'Voicing' );
+      }
+      if ( packageJSON.phet.simFeatures.supportsInteractiveHighlights ) {
+        features.push( 'Interactive Highlights' );
+      }
+    }
+  }
+  catch( e ) {} // some repos don't have package.json, and that's okay.
+  return features;
+};
 
 repos.forEach( repoName => {
   const responsibleDeveloper = responsibleDevObject[ repoName ].responsibleDevs.join( ',' )
   const responsibleDesigner = responsibleDevObject[ repoName ].responsibleDesigners.join( ',' )
-  responsibleTableString += `| ${repoName} | ${responsibleDeveloper} | ${responsibleDesigner} | \n`;
+  const features = getFeatures( repoName ).join( '<br/>' );
+  responsibleTableString += `| ${repoName} | ${responsibleDeveloper} | ${responsibleDesigner} | ${features} | \n`;
 } );
 
 fs.writeFileSync( './phet-info/sim-info/responsible_dev.md', responsibleTableString );
