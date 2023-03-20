1f954020288605a6883e4952efc497d2676735f9
Sam Reid <reids@colorado.edu>
2023-03-20 08:32:23 -0600

    Update scripts for caching issues, and outputting repos and users, see https://github.com/phetsims/phet-info/issues/207

--- github-dashboard/issueReport.js
+++ github-dashboard/issueReport.js
@@ -1,15 +1,28 @@
 // Copyright 2023, University of Colorado Boulder
 
-// eslint-disable-next-line require-statement-match
-const _ = require( 'lodash' );
+/**
+ * This script interacts with the GitHub API to fetch organization members, repositories, and open issues
+ * for the 'phetsims' organization. For users and repos, they are output to console where they can be saved to file manually.
+ * Issues are outputted to ./repodir.
+ * Key functionalities:
+ * * Get the list of organization members (getUsersFromGitHub)
+ * * Get the list of repositories (getReposFromGitHub)
+ * * Fetch open issues assigned to organization members, organized by repository (getAssignedIssuesByRepo)
+ * Usage: Run the script with Node.js. Optionally, set downloadNewData to true to download updated user and repo data.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Agustin Vallejo (PhET Interactive Simulations)
+ */
+
 const buildLocal = require( '../../perennial-alias/js/common/buildLocal' );
 const { Octokit } = require( 'octokit' ); // eslint-disable-line require-statement-match
 const octokit = new Octokit( {
   auth: buildLocal.phetDevGitHubAccessToken
 } );
+const fs = require( 'fs' );
 
 // List all users for the organization
-const getUsersFromGitHub = async () => { // eslint-disable-line no-unused-vars
+const getUsersFromGitHub = async () => {
   const result = await octokit.request( 'GET /orgs/phetsims/members', {
     org: 'phetsims',
     per_page: 100
@@ -20,103 +33,82 @@ const getUsersFromGitHub = async () => { // eslint-disable-line no-unused-vars
   return logins;
 };
 
-// Users that have >=1 ready for review issue as of Feb 16, 2023
-// const getUsersStatic = async () => {
-//   return [
-//     'AgustinVallejo',
-//     'amanda-phet',
-//     'arouinfar',
-//     'chrisklus',
-//     'emily-phet',
-//     'JacquiHayes',
-//     'jbphet',
-//     'jessegreenberg',
-//     'jonathanolson',
-//     'kathy-phet',
-//     'Luisav1',
-//     'matthew-blackman',
-//     'Matthew-Moore240',
-//     'mattpen',
-//     'oliver-phet',
-//     'pixelzoom',
-//     'samreid',
-//     'veillette',
-//     'zepumph'
-//   ];
-// };
+// Fetch the list of repositories in the organization
+const getReposFromGitHub = async () => {
+  const fetchRepos = async page => {
+    const result = await octokit.request( 'GET /orgs/phetsims/repos', {
+      org: 'phetsims',
+      per_page: 100,
+      page: page
+    } );
 
-const getDevUsersStatic = async () => {
-  return [
-    'AgustinVallejo',
-    'chrisklus',
-    'jbphet',
-    'jessegreenberg',
-    'jonathanolson',
-    'liammulh',
-    'Luisav1',
-    'marlitas',
-    'matthew-blackman',
-    'mattpen',
-    'pixelzoom',
-    'samreid',
-    'veillette',
-    'zepumph'
-  ];
+    return result;
+  };
+
+  let allRepos = [];
+  let currentPage = 1;
+  let fetchedRepos;
+
+  do {
+    fetchedRepos = await fetchRepos( currentPage );
+    allRepos = allRepos.concat( fetchedRepos.data );
+    currentPage += 1;
+  } while ( fetchedRepos.data.length === 100 );
+
+  const repoNames = allRepos.map( repo => repo.name );
+  return repoNames;
 };
 
-const getUsers = getDevUsersStatic();
+const repos = fs.readFileSync( './repos', 'utf8' ).split( '\n' );
+// const users = fs.readFileSync( './users', 'utf8' ).split( '\n' );
 
-// https://docs.github.com/en/rest/users/users?apiVersion=2022-11-28#list-users
-( async () => {
-  const users = await getUsers;
-  const results = [];
-  for ( let i = 0; i < users.length; i++ ) {
-    const user = users[ i ];
+// Fetch the open issues assigned to users in the organization, organized by repository
+const getAssignedIssuesByRepo = async () => {
 
-    const perPage = 100;
-    let page = 1;
-    let totalCount = 100;
-    const openIssuesResultPages = [];
+  const fetchIssues = async ( repo, page ) => {
+    const issues = await octokit.request( 'GET /search/issues', {
+      q: `repo:phetsims/${repo} is:issue is:open`,
+      per_page: 100,
+      page: page
+    } );
 
-    while ( totalCount + perPage >= page * perPage ) {
-      const openIssuesResult = await octokit.request( 'GET /search/issues', {
-        accept: 'application/vnd.github+json',
-        q: `is:issue is:open assignee:${user} org:phetsims`,
-        per_page: perPage,
-        page: page
-      } );
+    return issues;
+  };
 
-      openIssuesResultPages.push( openIssuesResult );
-      totalCount = openIssuesResult.data.total_count;
-      page += 1;
+  for ( const repo of repos ) {
+    let repoIssues = [];
+    let currentPage = 1;
+    let fetchedIssues;
+
+    do {
+      fetchedIssues = await fetchIssues( repo, currentPage );
+      repoIssues = repoIssues.concat( fetchedIssues.data.items );
+      currentPage += 1;
+    } while ( fetchedIssues.data.items.length === 100 );
+    try {
+      fs.mkdirSync( './repodir' );
     }
+    catch( e ) {
+      console.log( e );
+    }
+    fs.writeFileSync( `./repodir/${repo}`, JSON.stringify( repoIssues, null, 2 ) );
 
-    const openIssuesData = openIssuesResultPages.flat( 1 ).map( openIssuesResult => openIssuesResult.data );
-    const userTotalCount = openIssuesData[ 0 ].total_count;
+    // exit();
 
-    const totalReadyForReview = _.sumBy( openIssuesData, openIssuesData => {
-      let issueCount = 0;
-      openIssuesData.items.forEach( openIssue => {
-        openIssue.labels.forEach( label => {
-          issueCount += label.name === 'status:ready-for-review' ? 1 : 0;
-        } );
-      } );
-      return issueCount;
-    } );
+    // Exit from the node process
+    // process.exit( 0 );
+  }
+};
 
-    const totalHighPriority = _.sumBy( openIssuesData, openIssuesData => {
-      let issueCount = 0;
-      openIssuesData.items.forEach( openIssue => {
-        openIssue.labels.forEach( label => {
-          issueCount += label.name === 'priority:2-high' ? 1 : 0;
-        } );
-      } );
-      return issueCount;
-    } );
+( async () => {
+  const downloadNewData = false;
+  if ( downloadNewData ) {
+    const users = await getUsersFromGitHub();
+    console.log( users );
 
-
-    results.push( { user: user, 'open issues': userTotalCount, 'ready-for-review': totalReadyForReview, 'high-priority': totalHighPriority } );
+    const repos = await getReposFromGitHub();
+    console.log( repos.length, repos.join( '\n' ) );
   }
 
-  console.table( results, [ 'user', 'open issues', 'ready-for-review', 'high-priority' ] );
+  await getAssignedIssuesByRepo();
 } )();
---
+++ github-dashboard/processCachedIssues.js
@@ -0,0 +1,41 @@
+// Copyright 2023, University of Colorado Boulder
+
+/**
+ * Read the open issues cached to disk, and output the number of issues assigned to each user.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Agustin Vallejo (PhET Interactive Simulations)
+ */
+
+const fs = require( 'fs' );
+
+const repos = fs.readFileSync( './repos', 'utf8' ).split( '\n' );
+const users = fs.readFileSync( './users', 'utf8' ).split( '\n' );
+
+// Fetch the open issues assigned to users in the organization, organized by repository
+( () => {
+
+  users.forEach( user => {
+
+    let sum = 0;
+    // console.log( user);
+    const map = {};
+    repos.forEach( repo => {
+
+      const issues = JSON.parse( fs.readFileSync( `./repodir/${repo}`, 'utf8' ) );
+      const userIssues = issues.filter( issue => {
+        return issue.assignees.filter( assignee => {
+          return assignee.login === user;
+        } ).length > 0;
+      } );
+
+      if ( userIssues.length > 0 ) {
+        map[ repo ] = userIssues.length;
+      }
+
+      sum += userIssues.length;
+
+    } );
+    console.log( user, sum, JSON.stringify( map, null, 2 ) );
+  } );
+} )();
