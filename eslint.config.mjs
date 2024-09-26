a7841e917cfa5099da095474d13c537040e70172
Sam Reid <reids@colorado.edu>
2024-09-25 19:32:42 -0600

    Upgrade to ESLint 9 and flat config, see https://github.com/phetsims/chipper/issues/1474

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * ESlint configuration for phet-info.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import nodeEslintConfig from '../chipper/eslint/node.eslint.config.mjs';
+
+export default [
+  ...nodeEslintConfig
+];
