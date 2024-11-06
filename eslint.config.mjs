f4af44e19fbf30769b7e58e9914e1f47c4ea334d
Sam Reid <reids@colorado.edu>
2024-11-06 16:01:47 -0700

    Move files to eslint/config, see https://github.com/phetsims/perennial/issues/399

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@
  * @author Michael Kauzmann (PhET Interactive Simulations)
  */
 
-import nodeEslintConfig from '../perennial-alias/js/eslint/node.eslint.config.mjs';
+import nodeEslintConfig from '../perennial-alias/js/eslint/config/node.eslint.config.mjs';
 
 export default [
   ...nodeEslintConfig
