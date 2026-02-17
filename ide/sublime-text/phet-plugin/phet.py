ec5a05248aba6f231981f6860c5e88babe8570de
Jonathan Olson <jonathan.olson@colorado.edu>
2026-02-17 10:02:09 -0700

    Update Jonathan Olson author annotation, see https://github.com/phetsims/molecule-polarity/issues/305

--- ide/sublime-text/phet-plugin/phet.py
+++ ide/sublime-text/phet-plugin/phet.py
@@ -1,6 +1,6 @@
 # Copyright 2020, University of Colorado Boulder
 #
-# @author Jonathan Olson <jonathan.olson@colorado.edu>
+# @author Jonathan Olson (PhET Interactive Simulations)
 
 import json
 import os
